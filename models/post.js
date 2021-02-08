ebe4de4118868e0b85a32c73aedfe47ae32ce7fe
shahrour <ahmadshahrour951@gmail.com>
2021-02-08 22:09:32 +0400

    Users can see Subreddits

--- models/post.js
+++ models/post.js
@@ -5,19 +5,17 @@ const PostSchema = new Schema({
   title: { type: String, required: true },
   url: { type: String, required: true },
   summary: { type: String, required: true },
+  subreddit: { type: String, required: true },
   createdAt: { type: Date },
   updatedAt: { type: Date },
 });
 
 PostSchema.pre('save', function (next) {
-  // SET createdAt AND updatedAt
   const now = new Date();
   this.updatedAt = now;
-
   if (!this.createdAt) {
     this.createdAt = now;
   }
-
   next();
 });
 
