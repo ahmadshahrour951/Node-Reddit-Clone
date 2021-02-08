d1904aae42babcaca58dc2d04477210d0e076889
shahrour <ahmadshahrour951@gmail.com>
2021-02-08 20:25:31 +0400

    Users can submit and save posts

--- server.js
+++ server.js
@@ -1,11 +1,47 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
+require('./data/reddit-db');
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT;
+
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+
+// Handlebars
+const exphbs = require('express-handlebars');
+app.engine(
+  'hbs',
+  exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
+);
+app.set('view engine', 'hbs');
+
+// Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+//==================CONTROLLERS=======================\\
+require('./controllers/posts.js')(app);
 
 app.get('/', (req, res) => {
-  res.sendFile('public/index.html', { root: __dirname });
+  res.render('home');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get('/posts/new', (req, res) => {
+  res.render('posts-new')
+});
+
+//====================LISTEN============================\\
+module.exports = app;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port localhost:${PORT}!`);
 });
