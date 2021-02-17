93b7f34ac7514f63e7e2968f4d9437924d7b19ea
shahrour <ahmadshahrour951@gmail.com>
2021-02-17 04:38:00 +0400

    Users can log out

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,22 +9,28 @@ module.exports = app => {
   });
 
   // SIGN UP POST
-app.post("/sign-up", (req, res) => {
-  // Create User and JWT
-  const user = new User(req.body);
+  app.post('/sign-up', (req, res) => {
+    // Create User and JWT
+    const user = new User(req.body);
 
-  user
-    .save()
-    .then((user) => {
-      var token = jwt.sign({ _id: user._id }, process.env.SECRET, {
-        expiresIn: '60 days',
+    user
+      .save()
+      .then((user) => {
+        var token = jwt.sign({ _id: user._id }, process.env.SECRET, {
+          expiresIn: '60 days',
+        });
+        res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+        res.redirect('/');
+      })
+      .catch((err) => {
+        console.log(err.message);
+        return res.status(400).send({ err: err });
       });
-      res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
-      res.redirect('/');
-    })
-    .catch((err) => {
-      console.log(err.message);
-      return res.status(400).send({ err: err });
-    });
-});
+  });
+
+  // LOGOUT
+  app.get('/logout', (req, res) => {
+    res.clearCookie('nToken');
+    res.redirect('/');
+  });
 };
